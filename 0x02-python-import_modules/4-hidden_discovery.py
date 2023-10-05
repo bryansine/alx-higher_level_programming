234b0df5b17d1e949f5c3cfc105a21229ee47bf9
bryansine <bryansine1738@gmail.com>
2023-10-05 05:30:39 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    names_mod = dir(hidden_4)
-    filtered_names = [name for name in names_mod if not name.startswith("__")]
+import sys
+import hidden_4 as hidden
 
+if __name__ != "__main__":
+    exit()
+
+for name in dir(hidden):
+    if name[0:2] != "__":
+        print(name)
