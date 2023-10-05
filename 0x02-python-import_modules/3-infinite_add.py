695356b858157d5c6794b50907d74f1ad0285b04
bryansine <bryansine1738@gmail.com>
2023-10-05 05:28:04 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    length = len(sys.argv)
-    _sum = 0
-    for i in range(1, length):
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+argc = len(sys.argv) - 1
+
+i = 0
+result = 0
