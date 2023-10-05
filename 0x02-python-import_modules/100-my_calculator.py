8830e942c2f80af75add42ef4a14047ffcc83fe2
bryansine <bryansine1738@gmail.com>
2023-10-05 05:34:54 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-from sys import argv
-from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
-    exit()
+if __name__ == "__main__":
+    import calculator_1 as mod1
+    import sys
 
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-elif argv[2] == '+':
+    length = len(sys.argv)
