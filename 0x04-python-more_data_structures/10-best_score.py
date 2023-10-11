ee1ee2d12e8659a926bcb097448bef6dbcc4332d
bryansine <bryansine1738@gmail.com>
2023-10-11 04:53:48 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    """Function that returns key with the biggest integer value"""
-
-    if a_dictionary is None:
-        return None
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
