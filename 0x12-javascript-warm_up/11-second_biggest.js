6014044340930b745eed05a6d71b65cb02f0101e
bryansine <bryansine1738@gmail.com>
2024-03-11 08:16:40 -0700

    javascript- warm_up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
-if (process.argv.length <= 3){
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const s = process.argv.map(Number)
+  const args = process.argv.map(Number)
     .slice(2, process.argv.length)
-    .sort((x, y) => x - y);
-  console.log(s[s.length - 2]);
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-
-const x = process.argv[2];
-
-if (!parseInt(x)){
-	console.log("Missing size");
-}else {
-	for (let i = 0; i < x; i++){
-		let row = "";
-		for (let h = 0; h < x; h++)row+= "X";
-		console.log(row);
-	}
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
+  }
 }
