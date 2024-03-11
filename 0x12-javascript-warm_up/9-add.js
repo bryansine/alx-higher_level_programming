1868b8f98f0b9119efb8bfea284d7dda49898735
bryansine <bryansine1738@gmail.com>
2024-03-11 08:43:31 -0700

    javascript- warm_up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,6 @@
-function add(a, b) {
-    return a + b;
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
 }
 
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
-
-if (!isNaN(arg1) && !isNaN(arg2)) {
-    console.log("The addition of", arg1, "and", arg2, "is:", add(arg1, arg2));
-} else {
-    console.log("Please provide valid integer arguments.");
-}
-
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
