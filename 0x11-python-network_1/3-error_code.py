a45c69322b9b64e896d0ce99be2893645ee3cf88
bryansine <bryansine1738@gmail.com>
2024-03-28 14:42:33 -0700

    python Network

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,7 +8,7 @@
 
 if __name__ == "__main__":
     import sys
-    from urllib import request,error
+    from urllib import request, error
 
     try:
         with request.urlopen(sys.argv[1]) as res:
