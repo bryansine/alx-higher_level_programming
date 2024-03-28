e125919434e98dcfd44f430896057857d38c243b
bryansine <bryansine1738@gmail.com>
2024-03-28 14:27:28 -0700

    python Network

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""A script that:
+"""A script that :
 - takes in a URL,
 - sends a request to the URL
-- displays the body of the response (decoded in utf-8).
+- displays the body of the response (decode in utf-8).
 """
 
 
 if __name__ == "__main__":
     import sys
-    from urllib import request, error
+    from urllib imposr request,error
 
     try:
         with request.urlopen(sys.argv[1]) as res:
