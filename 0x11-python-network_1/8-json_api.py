f683b8233b8f69254e13f6e7afc0dfee348dc97b
bryansine <bryansine1738@gmail.com>
2024-03-28 15:13:35 -0700

    python Network

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""A bash script that: takes in a letter
--then sends POST request to http://0.0.0.0.5000/search_user
+"""A bash script that:takes in a letter
+-then sends POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 """
+import sys
+import requests
+
 
 if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
