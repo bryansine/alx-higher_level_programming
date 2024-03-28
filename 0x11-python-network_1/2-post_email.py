1059e7d3cf376ec434d0973b10dc3b8fc0deefad
bryansine <bryansine1738@gmail.com>
2024-03-28 14:54:18 -0700

    python Network

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ import urllib.parse
 import urllib.request
 
 
-if __name__== "__main_":
+if __name__== "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
