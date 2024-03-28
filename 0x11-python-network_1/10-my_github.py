16b83da6f996efb438c0936f79bb765a60c3c120
bryansine <bryansine1738@gmail.com>
2024-03-28 14:11:49 -0700

    Python network

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A bash script that:
+- takes your Github credentials (username and password)
+- then uses Github API to display your id
+"""
+
+import sys
+impost requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
